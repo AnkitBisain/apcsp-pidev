86047c295923207b9a3dfc481db9ba7c86d0b2ad
Ankit Bisain <ankit.bisain@gmail.com>
2019-12-17 13:13:04 -0800

    finished sizeofvariables

--- sizeofvariables.c
+++ sizeofvariables.c
@@ -11,9 +11,9 @@ int main()
 
   // print value and size of each variable
   printf("int a value: %d and size: %d bytes\n", a, sizeof(a));
- printf("double d value: %d and size: %d bytes\n", d, sizeof(d));
- printf("float f value: %d and size: %d bytes\n", f, sizeof(f));
- printf("char q value: %d and size: %d bytes\n", c, sizeof(c));
+ printf("double d value: %f and size: %d bytes\n", d, sizeof(d));
+ printf("float f value: %f and size: %d bytes\n", f, sizeof(f));
+ printf("char q value: %c and size: %d bytes\n", c, sizeof(c));
  printf("unsigned int u value: %d and size: %d bytes\n", u, sizeof(u));
  printf("short s value: %d and size: %d bytes\n", s, sizeof(s));
 
